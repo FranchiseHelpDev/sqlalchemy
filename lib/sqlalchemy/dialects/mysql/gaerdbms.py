23e1e887ab72d57e9779e7d488bac3aad029c5da
Kevin Dolan <norelxation@gmail.com>
2014-03-19 14:42:14 -0400

    ** hijack converters

--- lib/sqlalchemy/dialects/mysql/gaerdbms.py
+++ lib/sqlalchemy/dialects/mysql/gaerdbms.py
@@ -32,12 +32,77 @@ from .mysqldb import MySQLDialect_mysqldb
 from ...pool import NullPool
 import re
 
-print "pizza"
+# hijacking converters
+import datetime
+import types
+from google.storage.speckle.proto import jdbc_type
+from google.storage.speckle.python.api import converters
 
+def Str2Unicode(arg):
+    try:
+        return unicode(arg, 'utf-8')
+    except UnicodeDecodeError:
+        try:
+            return unicode(arg, 'latin-1')
+        except UnicodeDecodeError:
+            return "[ERROR]"
+
+CONVERTERS = {
+
+    types.IntType: converters.Any2Str,
+    types.LongType: converters.Any2Str,
+    types.FloatType: converters.Any2Str,
+    types.TupleType: converters.Tuple2Str,
+    types.BooleanType: converters.Bool2Str,
+    types.StringType: converters.Any2Str,
+    types.UnicodeType: converters.Unicode2Str,
+    datetime.date: converters.Date2Str,
+    datetime.datetime: converters.Datetime2Str,
+    datetime.time: converters.Time2Str,
+    converters.Blob: converters.Any2Str,
+
+
+    jdbc_type.BIT: int,
+    jdbc_type.SMALLINT: int,
+    jdbc_type.INTEGER: int,
+    jdbc_type.BIGINT: int,
+    jdbc_type.TINYINT: int,
+    jdbc_type.REAL: float,
+    jdbc_type.DOUBLE: float,
+    jdbc_type.NUMERIC: float,
+    jdbc_type.DECIMAL: float,
+    jdbc_type.FLOAT: float,
+    jdbc_type.CHAR: Str2Unicode,
+    jdbc_type.VARCHAR: Str2Unicode,
+    jdbc_type.LONGVARCHAR: Str2Unicode,
+    jdbc_type.DATE: converters.Str2Date,
+    jdbc_type.TIME: converters.Str2Time,
+    jdbc_type.TIMESTAMP: converters.Str2Datetime,
+    jdbc_type.BINARY: converters.Blob,
+    jdbc_type.VARBINARY: converters.Blob,
+    jdbc_type.LONGVARBINARY: converters.Blob,
+    jdbc_type.BLOB: converters.Blob,
+    jdbc_type.CLOB: Str2Unicode,
+    jdbc_type.NCLOB: Str2Unicode,
+    jdbc_type.NCHAR: Str2Unicode,
+    jdbc_type.NVARCHAR: Str2Unicode,
+    jdbc_type.LONGNVARCHAR: Str2Unicode,
+
+    jdbc_type.ARRAY: Str2Unicode,
+    jdbc_type.NULL: Str2Unicode,
+    jdbc_type.OTHER: Str2Unicode,
+    jdbc_type.JAVA_OBJECT: Str2Unicode,
+    jdbc_type.DISTINCT: Str2Unicode,
+    jdbc_type.STRUCT: Str2Unicode,
+    jdbc_type.REF: Str2Unicode,
+    jdbc_type.DATALINK: Str2Unicode,
+    jdbc_type.BOOLEAN: Str2Unicode,
+    jdbc_type.ROWID: Str2Unicode,
+    jdbc_type.SQLXML: Str2Unicode,
+}
 
 def _is_dev_environment():
     return os.environ.get('SERVER_SOFTWARE', '').startswith('Development/')
-
 
 class MySQLDialect_gaerdbms(MySQLDialect_mysqldb):
 
@@ -73,6 +138,7 @@ class MySQLDialect_gaerdbms(MySQLDialect_mysqldb):
             # the traditional google.api.rdbms wrapper
             opts['dsn'] = ''
             opts['instance'] = url.query['instance']
+            opts['conv'] = CONVERTERS
         return [], opts
 
     def _extract_error_code(self, exception):
